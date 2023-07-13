9fe1d404aa25b81d8aed3cea7353a5bd89a8a297
griffinw@protonmail.com <griffinw@protonmail.com>
2023-07-13 19:20:47 -0400

    added home.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+const Home = () => {
+  const { loading, data } = useQuery();
+  // const thoughts = data?.thoughts || [];
+  // example
+  return (
+    <main>
+      <div className="">
+        <div
+          className=""
+          style={{ }}
+        >
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Home;
