7b9812c61dd701eee8a957d643267e999c005058
griffinw@protonmail.com <griffinw@protonmail.com>
2023-07-12 21:11:35 -0400

    updated navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -38,8 +38,8 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          <span role="img" aria-label="shopping bag"></span>
+          Mental Mastery
         </Link>
       </h1>
 
