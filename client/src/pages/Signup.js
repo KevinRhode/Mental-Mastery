11605e95348e78b29421512fa997019795bff430
griffinw@protonmail.com <griffinw@protonmail.com>
2023-07-16 17:38:38 -0400

    styled, and made the sign up page more responsive. It looks great

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -5,7 +5,7 @@ import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
 
 function Signup(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ firstName: '', lastName: '', email: '', password: '' });
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
@@ -14,7 +14,7 @@ function Signup(props) {
       variables: {
         email: formState.email,
         password: formState.password,
-        username: formState.firstName +' '+formState.lastName,
+        username: formState.firstName + ' ' + formState.lastName,
       },
     });
     const token = mutationResponse.data.register.token;
@@ -30,55 +30,81 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+    <div
+      className="container my-1"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        padding: '20px',
+        boxSizing: 'border-box',
+      }}
+    >
+      <div style={{ maxWidth: '400px', width: '100%' }}>
+        <Link to="/login" style={{ display: 'block', marginBottom: '10px', textAlign: 'center' }}>
+          ← Go to Login
+        </Link>
 
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+        <h2 style={{ textAlign: 'center' }}>Signup</h2>
+        <form onSubmit={handleFormSubmit}>
+          <div style={{ marginBottom: '10px' }}>
+            <label htmlFor="firstName" style={{ display: 'block', marginBottom: '5px' }}>
+              First Name:
+            </label>
+            <input
+              placeholder="First"
+              name="firstName"
+              type="text"
+              id="firstName"
+              style={{ width: '100%', padding: '5px' }}
+              onChange={handleChange}
+            />
+          </div>
+          <div style={{ marginBottom: '10px' }}>
+            <label htmlFor="lastName" style={{ display: 'block', marginBottom: '5px' }}>
+              Last Name:
+            </label>
+            <input
+              placeholder="Last"
+              name="lastName"
+              type="text"
+              id="lastName"
+              style={{ width: '100%', padding: '5px' }}
+              onChange={handleChange}
+            />
+          </div>
+          <div style={{ marginBottom: '10px' }}>
+            <label htmlFor="email" style={{ display: 'block', marginBottom: '5px' }}>
+              Email:
+            </label>
+            <input
+              placeholder="youremail@test.com"
+              name="email"
+              type="email"
+              id="email"
+              style={{ width: '100%', padding: '5px' }}
+              onChange={handleChange}
+            />
+          </div>
+          <div style={{ marginBottom: '10px' }}>
+            <label htmlFor="pwd" style={{ display: 'block', marginBottom: '5px' }}>
+              Password:
+            </label>
+            <input
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              style={{ width: '100%', padding: '5px' }}
+              onChange={handleChange}
+            />
+          </div>
+          <div style={{ textAlign: 'right' }}>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
