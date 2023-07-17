6951244fa646d960a1fe17d5be4fddca0c917088
Little Bit <cthibodeau09@gmail.com>
2023-07-17 19:08:46 -0400

    add: adding mutation imports for creating a task

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Auth from "../utils/auth";
+import { useMutation } from "@apollo/client";
+import { CREATE_TASK } from "../utils/mutations";
 
 class Tasks extends Component {
 
