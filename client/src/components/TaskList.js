9df85845ead4d5aeaa333b3f1b03509b4cd0bf78
Little Bit <cthibodeau09@gmail.com>
2023-07-19 22:35:09 -0400

    add: added to Tasklist return function

---
+++ client/src/components/TaskList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function TaskList(props) {
+    return (
+        <ul style={{ listStyle:'none'}}
+        className="list-group">
+            {props.results.map((result) => (
+                <li className="list-group-item"
+                key={result.id}>
+                    {/* {result._id}. */}
+                    {result.taskname},
+                    {result.location},
+                    {result.date}
+                </li>
+            ))}
+        </ul>
+    );
+}
