f5b96377444d5d80f83c424c901af2b145cb4a51
KevinRhode <kevinrhode@hotmail.com>
2023-07-22 17:35:04 -0400

    add: basic div to start testing

--- client/src/components/ProfileIcon.js
+++ client/src/components/ProfileIcon.js
@@ -1,11 +1,34 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+const styles = StyleSheet.create({
+    container: {
+      paddingTop: 50,
+    },
+    tinyLogo: {
+      width: 50,
+      height: 50,
+    },
+    logo: {
+      width: 66,
+      height: 58,
+    },
+  });
 
 function ProfileIcon(props){
+
     const navigate = useNavigate();
+
     const linkToFamilyUser = (id) =>{
         navigate(`/familyuser/${id}`);
     }
+
+    return (
+        <div onClick={linkToFamilyUser(props.id)}>
+            <img src={logo} style={styles.tinyLogo} className='profileIcon' alt='profileIcon'/>
+        </div>
+    );
 
 }
 
